c3a28c6c558c30e0919a8e4cb40b8a97a447824a
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-07-12 14:41:29 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -78,20 +78,17 @@ end
 
 
 
-  
-def play_song
+  def play_song
 puts "Which song number would you like to play?"
 user_input = gets.chomp
 song_list_genre = Song.all[user_input.to_i - 1] 
-  # binding.pry
-song_list_genre.sort {|s1,s2| s1.name <=> s2.name}.each_with_index{ |song,index| 
-  puts "#{index+1}. #{song.artist.name} - #{song.name}"}
-end    
- 
- 
-song_list_genre = Song.all.select { |song| song.name == user_input.name}
+
+  puts "#{user_input}. #{song_list_genre.artist.name} - #{song_list_genre.name}"}
+
+end
  
 
+  
   end 
 end 
 
